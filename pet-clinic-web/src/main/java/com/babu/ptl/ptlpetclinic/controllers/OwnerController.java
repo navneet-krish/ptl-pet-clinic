21e6f96a0b60dce989c0165f31cab5da8c1da5f9
`navneet <navneet.gs@gmail.com>
2022-01-22 08:51:45 +0530

    Getting the data from bootstrap data into html

--- pet-clinic-web/src/main/java/com/babu/ptl/ptlpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/babu/ptl/ptlpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.babu.ptl.ptlpetclinic.controllers;
 
+import com.babu.ptl.ptlpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","index","index.html"})
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/ownerindex";
     }
