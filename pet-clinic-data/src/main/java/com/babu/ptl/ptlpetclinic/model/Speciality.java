c05f408d18bc654a8f4dcea104d88e59e8d79a5b
`navneet <navneet.gs@gmail.com>
2022-01-25 05:07:30 +0530

    Added Speciality and associated with vets

---
+++ pet-clinic-data/src/main/java/com/babu/ptl/ptlpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.babu.ptl.ptlpetclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
