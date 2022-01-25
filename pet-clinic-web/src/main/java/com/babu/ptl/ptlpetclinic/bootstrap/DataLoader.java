c8b5f7874f02f2ba861b64f876d7d63f1c086d66
`navneet <navneet.gs@gmail.com>
2022-01-25 05:41:40 +0530

    Added PetType Data on startup

--- pet-clinic-web/src/main/java/com/babu/ptl/ptlpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/babu/ptl/ptlpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.babu.ptl.ptlpetclinic.bootstrap;
 
 import com.babu.ptl.ptlpetclinic.model.Owner;
+import com.babu.ptl.ptlpetclinic.model.PetType;
 import com.babu.ptl.ptlpetclinic.model.Vet;
 import com.babu.ptl.ptlpetclinic.services.OwnerService;
+import com.babu.ptl.ptlpetclinic.services.PetTypeService;
 import com.babu.ptl.ptlpetclinic.services.VetService;
 import com.babu.ptl.ptlpetclinic.services.map.OwnerServiceMap;
 import com.babu.ptl.ptlpetclinic.services.map.VetServiceMap;
@@ -15,15 +17,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Navneet");
