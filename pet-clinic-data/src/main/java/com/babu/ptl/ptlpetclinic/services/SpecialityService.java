40b6a4b3c65df7503820bb83af49978d96791285
`navneet <navneet.gs@gmail.com>
2022-01-25 06:59:07 +0530

    Added Speciality service

---
+++ pet-clinic-data/src/main/java/com/babu/ptl/ptlpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.babu.ptl.ptlpetclinic.services;
+
+import com.babu.ptl.ptlpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/babu/ptl/ptlpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package com.babu.ptl.ptlpetclinic.services.map;
+
+import com.babu.ptl.ptlpetclinic.model.Speciality;
+import com.babu.ptl.ptlpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
